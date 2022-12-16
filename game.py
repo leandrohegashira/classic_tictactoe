ba80cefc0c91e69f8f71479a6e5aae73bcc6ea3a
Leandro <leandrohegashira>
2022-12-16 19:46:51 -0300

    Sending new function to print message of winning game

--- game.py
+++ game.py
@@ -26,45 +26,45 @@ def game():
     board[position] == board[x]
     board[position] = turn
     printBoard(board)
-    if turn == 'x':
-        turn = 'o'
-    else:
-        turn = 'x'
-
-
+    
 
     # Check if player wons or it's a tie
     if(board['7'] == board['8'] == board['9'] != ' '):
       printBoard(board)
-      print('Player wons') 
+      print(f'Player "{turn}" won') 
       break
     elif(board['4'] == board['5'] == board['6'] != ' '):
       printBoard(board)
-      print('Player wons')
+      print(f'Player "{turn}" won')
       break
     elif(board['1'] == board['2'] == board['3'] != ' '):
       printBoard(board)
-      print('Player wons')
+      print(f'Player "{turn}" won')
       break
     elif(board['1'] == board['4'] == board['7'] != ' '):
       printBoard(board)
-      print('Player wons')
+      print(f'Player "{turn}" won') 
       break
     elif(board['2'] == board['5'] == board['8'] != ' '):
       printBoard(board)
-      print('Player wons')
+      print(f'Player "{turn}" won')
       break
     elif(board['3'] == board['6'] == board['9'] != ' '):
       printBoard(board)
-      print('Player wons')
+      print(f'Player "{turn}" won')
     elif(board['1'] == board['5'] == board['6'] != ' '):
       printBoard(board)
-      print('Player wons')
+      print(f'Player "{turn}" won')
       break
     elif(board['3'] == board['5'] == board['7'] != ' '):
       printBoard(board)
-      print('Player wons')
+      print(f'Player "{turn}" won')
       break
+
+    if turn == 'x':
+          turn = 'o'
+    else:
+          turn = 'x'
 
   # Repeat game 
   
