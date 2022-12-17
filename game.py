24b28f4fe38f3f553d769632af34d863355c2c59
Leandro <leandrohegashira>
2022-12-17 11:25:04 -0300

    Sending new updates in the code

--- game.py
+++ game.py
@@ -16,7 +16,10 @@ def playAgain():
   repeat = input('Do you want to play again y/n? ')
 
   while repeat == 'y':
+    for key in board:
+      board[key] = ' '
     game()
+    playAgain()
   quit()  
 
 
@@ -26,56 +29,59 @@ printBoard(board)
 def game():
 
   turn = 'x'
+  count = 1
     
   for x in board:
-    position = input('Wich position do you want to mark: ')
 
-    board[position] == board[x]
-    board[position] = turn
-    printBoard(board)
-    
+    while count < 10:
 
-    # Check wich player won
-    if(board['7'] == board['8'] == board['9'] != ' '):
+      position = input('Wich position do you want to mark: ')
+      board[position] == board[x]
+      board[position] = turn
       printBoard(board)
-      print(f'Player "{turn}" won') 
-      break
-    elif(board['4'] == board['5'] == board['6'] != ' '):
-      printBoard(board)
-      print(f'Player "{turn}" won')
-      break
-    elif(board['1'] == board['2'] == board['3'] != ' '):
-      printBoard(board)
-      print(f'Player "{turn}" won')
-      break
-    elif(board['1'] == board['4'] == board['7'] != ' '):
-      printBoard(board)
-      print(f'Player "{turn}" won') 
-      break
-    elif(board['2'] == board['5'] == board['8'] != ' '):
-      printBoard(board)
-      print(f'Player "{turn}" won')
-      break
-    elif(board['3'] == board['6'] == board['9'] != ' '):
-      printBoard(board)
-      print(f'Player "{turn}" won')
-    elif(board['1'] == board['5'] == board['6'] != ' '):
-      printBoard(board)
-      print(f'Player "{turn}" won')
-      break
-    elif(board['3'] == board['5'] == board['7'] != ' '):
-      printBoard(board)
-      print(f'Player "{turn}" won')
-      break
+      count+=1
 
-    if turn == 'x':
-      turn = 'o'
-    else:
-      turn = 'x'
+      # Check wich player won
+      if(board['7'] == board['8'] == board['9'] != ' '):
+        printBoard(board)
+        print(f'Player "{turn}" won') 
+        break
+      elif(board['4'] == board['5'] == board['6'] != ' '):
+        printBoard(board)
+        print(f'Player "{turn}" won')
+        break
+      elif(board['1'] == board['2'] == board['3'] != ' '):
+        printBoard(board)
+        print(f'Player "{turn}" won')
+        break
+      elif(board['1'] == board['4'] == board['7'] != ' '):
+        printBoard(board)
+        print(f'Player "{turn}" won') 
+        break
+      elif(board['2'] == board['5'] == board['8'] != ' '):
+        printBoard(board)
+        print(f'Player "{turn}" won')
+        break
+      elif(board['3'] == board['6'] == board['9'] != ' '):
+        printBoard(board)
+        print(f'Player "{turn}" won')
+        break
+      elif(board['1'] == board['5'] == board['6'] != ' '):
+        printBoard(board)
+        print(f'Player "{turn}" won')
+        break
+      elif(board['3'] == board['5'] == board['7'] != ' '):
+        printBoard(board)
+        print(f'Player "{turn}" won')
+        break
 
+      # Swich the turn of players
+      if turn == 'x':
+        turn = 'o'
+      else:
+        turn = 'x'
 
-  
-
+    break
 
 game()
 playAgain()
